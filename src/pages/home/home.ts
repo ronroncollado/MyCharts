d79f6c79ca5a62c5f6ebb3b0e9bf9be9185b269a
ronroncollado <ronroncollado@gmail.com>
2017-04-10 22:52:38 +0800

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -94,10 +94,10 @@ export class HomePage {
  
             type: 'line',
             data: {
-                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                 datasets: [
                     {
-                        label: "My First dataset",
+                        label: '# of Votes',
                         fill: false,
                         lineTension: 0.1,
                         backgroundColor: "rgba(75,192,192,0.4)",
@@ -115,7 +115,7 @@ export class HomePage {
                         pointHoverBorderWidth: 2,
                         pointRadius: 1,
                         pointHitRadius: 10,
-                        data: [65, 59, 80, 81, 56, 55, 40],
+                        data: [65, 59, 80, 81, 56, 55],
                         spanGaps: false,
                     }
                 ]
